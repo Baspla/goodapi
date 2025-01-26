8939babead85f4ff6fdf284078c3cf229246ba25
Tim Morgner <tim@timmorgner.de>
2025-01-26 19:35:02 +0100

    added legacy import

--- src/routes/v1/debug.ts
+++ src/routes/v1/debug.ts
@@ -1,6 +1,14 @@
 import express, {NextFunction, Request, Response} from 'express';
 import {requireAdmin, requireAuth} from "../../middleware/auth.js";
-import {createUser, deleteAllUsers, deleteUser, getUsers, setUserAdmin} from "../../db/operations/users.js";
+import {
+    createUser,
+    deleteAllUsers,
+    deleteUser,
+    getUserByDiscordId,
+    getUsers,
+    NewUser,
+    setUserAdmin, User
+} from "../../db/operations/users.js";
 import {
     createRecommendation, deleteRecommendation, getRecommendationById, getRecommendations,
     getRecommendationsByUserId
@@ -59,6 +67,73 @@ router.get('/stats', async function (req: Request, res: Response) {
         tags: await getTags(),
     });
 });
+
+router.post('/legacyUsers', async function (req: Request, res: Response) {
+    const users = req.body;
+    if (!users) {
+        res.status(400).json({error: 'No users provided'});
+        return;
+    }
+    try {
+        for (const userdata of users) {
+            let user : NewUser = {
+                discordId: userdata.discord,
+                username: userdata.username,
+                email: userdata.email,
+                role: 'user'
+            }
+            await createUser(user);
+        }
+        res.json({ok: true});
+    } catch (error) {
+        console.error('Error creating users:', error);
+        res.status(500).json({error: 'Error creating users'});
+    }
+})
+
+router.post('/legacyRecommendations', async function (req: Request, res: Response) {
+    /*
+    {
+    "author": "3jwcaf23vjxgdpn",
+    "created": "2024-05-01 19:34:13.537Z",
+    "description": "Cool Youtube Channel",
+    "id": "azgmzhe3yolcuey",
+    "image_url": "https://yt3.googleusercontent.com/ytc/AIdro_nxrDGcxMGo8yKf2_Dw0eaGEWj39IAIdZQjAuz-_mBHjUI=s900-c-k-c0x00ffffff-no-rj",
+    "is_request": 0,
+    "title": "CGP Grey",
+    "topic": "uh3nqw4tmbpujct",
+    "updated": "2024-05-01 19:34:13.537Z",
+    "url": "https://www.youtube.com/@CGPGrey",
+    "discord": "197433566102028288"
+  }
+     */
+    const recommendations = req.body;
+    if (!recommendations) {
+        res.status(400).json({error: 'No recommendations provided'});
+        return;
+    }
+    try {
+        for (const recommendation of recommendations) {
+            //find user by discord id
+            const user:User|undefined = await getUserByDiscordId(recommendation.discord);
+            if (!user) {
+                console.error('User not found:', recommendation.discord, ' for recommendation:', recommendation.title);
+                continue;
+            }
+            await createRecommendation({
+                userId: user.id,
+                title: recommendation.title,
+                url: recommendation.url,
+                imageUrl: recommendation.image_url,
+                createdAt: new Date(recommendation.created),
+            });
+        }
+        res.json({ok: true});
+    } catch (error) {
+        console.error('Error creating recommendations:', error);
+        res.status(500).json({error: 'Error creating recommendations'});
+    }
+})
 
 router.get('/setupSampleData', async function (req: Request, res: Response) {
     try {
